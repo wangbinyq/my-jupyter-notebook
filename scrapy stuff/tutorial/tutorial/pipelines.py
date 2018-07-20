20e26a70a6981bcfc29936e8c7fe47bfb538a2a9
wangb <wangbin02@360.net>
2018-07-20 14:39:52 +0800

    add sqlite3 processing in another thread

--- scrapy stuff/tutorial/tutorial/pipelines.py
+++ scrapy stuff/tutorial/tutorial/pipelines.py
@@ -6,7 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from .models import db, Quote, Tag, QuoteTag, Author
-
+from concurrent.futures import ThreadPoolExecutor
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
@@ -17,12 +17,13 @@ class Sqlite3Pipeline(object):
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            db_uri=crawler.settings.get('DB_URI', ':memory:'),
+            db_uri=crawler.settings.get('DB_URI', ':memory:')
         )
 
-    def __init__(self, db_uri):
+    def __init__(self, db_uri, threadings):
         self.db = db
         db.init(db_uri)
+        self.threadpool = ThreadPoolExecutor(max_workers=1)
 
     def open_spider(self, spider):
         self.db.connect()
@@ -30,8 +31,9 @@ class Sqlite3Pipeline(object):
 
     def close_spider(self, spider):
         self.db.close()
+        self.threadpool.shutdown(wait=True)
 
-    def process_item(self, item, spider):
+    def process_item_thread(self, item, spider):
         name = item.get('author', '')
         text = item.get('text', '')
         tags = item.get('tags', [])
@@ -42,4 +44,7 @@ class Sqlite3Pipeline(object):
             tag, created = Tag.get_or_create(name=t)
             QuoteTag.get_or_create(tag=tag, quote=quote)
 
+
+    def process_item(self, item, spider):
+        self.threadpool.submit(self.process_item_thread, item, spider)
         return item
